8a9ea9fb6d82b976e6959269865e3918f4c6cd42
Haddad Eid <eidqhaddad@gmail.com>
2018-12-08 00:29:50 +0000

    Versao final aula 9

--- webapp/Component.js
+++ webapp/Component.js
@@ -20,6 +20,18 @@ sap.ui.define([
 			// call the base component's init function
 			UIComponent.prototype.init.apply(this, arguments);
 
+           // jQuery.sap.log.setLevel(jQuery.sap.log.Level.ALL);
+
+			this.getRouter().attachRouteMatched(function (oEvent) {
+				var sRoute = oEvent.getParameters().name;
+				console.log(sRoute);
+
+			});
+
+			this.getRouter().getRoute("exibir").attachPatternMatched(function (oEvent) {
+				jQuery.sap.log.warning("Tela de exibição aberta");
+			});
+
 			// enable routing
 			this.getRouter().initialize();
 
--- webapp/controller/S2.controller.js
+++ webapp/controller/S2.controller.js
@@ -2,6 +2,22 @@ sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
 	"use strict";
 	return Controller.extend("ovly.navegacao.controller.S2", {
 
+		onItemPress: function (oEvent) {
+			
+			var oParameters = oEvent.getParameters();
+			
+			// @type sap.m.StandardListItem
+			var oItemPressed = oParameters.listItem;
+			var sTitle = oItemPressed.getTitle();
+			
+			// alert(sTitle);
+			var oRouter = this.getOwnerComponent().getRouter();
+			oRouter.navTo("exibir", {
+				identificador: sTitle,
+			},true);
+			
+		},
+		
 		onPressAdd: function (oEvent) {
 
 			var oRouter = this.getOwnerComponent().getRouter();
